775b6acf181657e296657f9989fbf04f3856d9c9
pleshkovvli <pleshkovvli@gmail.com>
2018-03-03 01:57:31 +0700

    Add tests.

--- problems-1/v-pleshkov/task5.py
+++ problems-1/v-pleshkov/task5.py
@@ -1,21 +1,45 @@
 import unittest
 
+import itertools
+
 from primes import is_prime, prime_numbers_fast
 from sys import argv
 
 
 def prime_numbers(limit):
-    return [2] + [n for n in range(3, limit, 2) if is_prime(n)]
+    return [n for n in range(2,3) if limit >= 2] + [n for n in range(3, limit + 1, 2) if is_prime(n)]
+
+
+class TestEratosthenesPrimes(unittest.TestCase):
+    def testEdgeCases(self):
+        self.assertEqual(prime_numbers(0), [])
+        self.assertEqual(prime_numbers(1), [])
+        self.assertEqual(prime_numbers(-1), [])
+        self.assertEqual(prime_numbers(-2), [])
+        self.assertEqual(prime_numbers(-231), [])
+
+    def testPrimes(self):
+        self._testPrimes(3342)
+        self._testPrimes(5232)
+        self._testPrimes(92313)
+        self._testPrimes(2213)
+        self._testPrimes(4121)
+
+    def _testPrimes(self, limit):
+        primes = prime_numbers(limit)
+        self.assertEqual(primes, [n for n in itertools.islice(prime_numbers_fast(), len(primes))])
 
 
 def main():
     if len(argv) < 2:
-        print(f"Usage: python3 {__file__} LIMIT")
-        return
-    if argv[1] == "test":
         unittest.main()
-    else:
-        print(prime_numbers(int(argv[1])))
+        return
+
+    try:
+        limit = int(argv[1])
+        print([n for n in prime_numbers(limit)])
+    except ValueError:
+        print("Integer number expected as argument")
 
 
 if __name__ == "__main__":
